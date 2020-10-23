73515c6c3e18bda4deea2e42d5e638f9e80384f2
Tim-VandenBorre <69650471+Tim-VandenBorre@users.noreply.github.com>
2020-10-23 13:47:58 +0200

    Initial commit

---
+++ werkstuk/src/main/java/be/ehb/werkstuk/WerkstukApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.werkstuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerkstukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerkstukApplication.class, args);
+    }
+
+}
